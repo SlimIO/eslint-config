e181a1ab56d02a8e4acf3698a56d75a640a0d09e
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2018-11-22 23:41:02 +0100

    refactor: add new set of rules

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-module.exports = require('./eslintrc.json');
+"use strict";
+const { join } = require("path");
+
+// CONSTANTS
+const RULES_DIR = join(__dirname, "rules");
+
+module.exports = {
+    extends: [
+        join(RULES_DIR, "possible-errors.js"),
+        join(RULES_DIR, "best-practices.js"),
+        join(RULES_DIR, "node.js"),
+        join(RULES_DIR, "styles.js"),
+        join(RULES_DIR, "ecmascript6.js")
+    ].map(require.resolve),
+    parserOptions: {
+        ecmaVersion: 9,
+        ecmaFeatures: {
+            impliedStrict: true
+        },
+        sourceType: "module"
+    }
+}
